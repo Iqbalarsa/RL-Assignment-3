653d060bc3fb687a986dd3e8b9337bf000ca6864
Iqbalarsa <iqbalarsa99@gmail.com>
2026-04-21 18:46:09 +0200

    Update SharedNetwork.py

--- SharedNetwork.py
+++ SharedNetwork.py
@@ -4,7 +4,7 @@ import torch.nn.functional as F
 
 class Policy(nn.Module):
     
-    def __init__(self, state_dim, action_dim, hidden_dim1 = 64):
+    def __init__(self, state_dim, action_dim, critic_dim = 1, hidden_dim1 = 64):
         super(Policy, self).__init__()
         
         # Hidden Layer
@@ -19,7 +19,7 @@ class Policy(nn.Module):
         self.actor_head = nn.Linear(hidden_dim1, action_dim)
         
         # Critic Ouput layer
-        self.critic_head = nn.Linear(hidden_dim1, 1)
+        self.critic_head = nn.Linear(hidden_dim1, critic_dim)
         
     def forward(self, x):
         x = self.hidden_layers(x)
